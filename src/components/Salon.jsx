842b0e8cb2edcc86e454c999964d94c3668fee29
jordan <hutswing12@gmail.com>
2026-06-11 20:57:20 +0900

    260611

--- src/components/Salon.jsx
+++ src/components/Salon.jsx
@@ -23,7 +23,33 @@ function Salon({ open, onClose }) {
       <button type="button" className="salon_close" onClick={onClose} aria-label="Close salon popup">
         <span></span><span></span>
       </button>
-      <div className="salon_popup_page" role="dialog" aria-modal="true" aria-label="Salon"></div>
+      <div className="salon_popup_page" role="dialog" aria-modal="true" aria-label="Salon">
+        <div className="head b-b b-2 b-c-white">
+          <div className="apprael display-m b-r b-c-white head_left">SALON</div>
+          <div className="head_right">
+            <div className="search_w b-t b-c-white">
+              <input className="head-m fw-sm" type="text" placeholder="지역 or 지점명 검색" />
+              <button><img src="/img/icon_search.svg" alt="" /></button>
+            </div>
+          </div>
+        </div>
+        <div className="body">
+          <div className="body_left">
+            <div className="b_l_l">
+              <ol className="head-m">
+                <li>서울</li>
+                <li>경기</li>
+                <li>지방</li>
+              </ol>
+              <a className="arc_logo"><img src="/img/arc.png" alt="" /></a>
+            </div>
+            
+            <ul className="b_l_r"></ul>
+          </div>
+          <div className="body_right"></div>
+
+        </div>
+      </div>
     </div>
   );
 }
