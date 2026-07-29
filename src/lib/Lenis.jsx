07dc6f9be421dba14156e15df46ca5b54dd3bdb2
jordan <hutswing12@gmail.com>
2026-07-29 18:16:07 +0900

    fix: restore global Lenis scroll settings

--- src/lib/Lenis.jsx
+++ src/lib/Lenis.jsx
@@ -1,34 +1,26 @@
 import { useEffect } from "react";
 import Lenis from "lenis";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
 
 export default function LenisProvider() {
   useEffect(() => {
     const lenis = new Lenis({
-      lerp: 0.08,
-      wheelMultiplier: 1.4,
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      wheelMultiplier: 1,
     });
 
     window.lenis = lenis;
-    lenis.on("scroll", ScrollTrigger.update);
 
-    const updateLenis = (time) => {
-      lenis.raf(time * 1000);
-    };
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
 
-    gsap.ticker.add(updateLenis);
-    gsap.ticker.lagSmoothing(0);
+    requestAnimationFrame(raf);
 
     return () => {
-      lenis.off("scroll", ScrollTrigger.update);
-      gsap.ticker.remove(updateLenis);
       lenis.destroy();
-      if (window.lenis === lenis) {
-        window.lenis = null;
-      }
+      window.lenis = null;
     };
   }, []);
 
