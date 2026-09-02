808338f723ac9c85ae56af77199b467dd5ac8dbb
jordan <hutswing12@gmail.com>
2026-09-02 11:43:44 +0900

    260902

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -133,7 +133,7 @@ function Header() {
           </div>
 
           <div className="h_nav_group_3">
-            <span className="gt display-xs">id Magazine</span>
+            <TransitionLink className="gt display-xs" to="/magazine/id-news" onClick={() => setNavOpen(false)}>id Magazine</TransitionLink>
             <ul className="body-s">
               <li><TransitionLink to="/magazine/id-news" onClick={() => setNavOpen(false)}>id NEWS</TransitionLink></li>
               <li><TransitionLink to="/magazine/id-event" onClick={() => setNavOpen(false)}>id EVENT</TransitionLink></li>
